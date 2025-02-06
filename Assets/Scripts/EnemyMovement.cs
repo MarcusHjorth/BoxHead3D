ae1786e86df31d67e7d1761cb865cfc63cd568c7
Marcus Rasmussen <marcusphr1808@gmail.com>
2025-02-06 19:36:11 +0100

    A bunch of stuff that doesn't work

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,11 +6,24 @@ public class EnemyMovement : MonoBehaviour
     public Transform player;
 
     private NavMeshAgent navMeshAgent;
+    public PlayerSys playerSys;
+    public int damage = 2;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+    
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerSys.TakeDamage(damage);
+        }
+    }
+    
 
     // Update is called once per frame
     void Update()
