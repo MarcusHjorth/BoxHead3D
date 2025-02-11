39815216d28cb1516de3b746ac21de618704c421
MateenCPH <cph-mr645@cphbusiness.dk>
2025-02-11 12:29:25 +0100

    pickups

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -5,8 +6,14 @@ public class PowerUp : MonoBehaviour
 {
     public float multiplier = 1.4f;
     public float duration = 4f;
+    private RandomSpawner randomSpawner;
 
     public GameObject pickupEffect;
+
+    private void Start()
+    {
+        randomSpawner = FindAnyObjectByType<RandomSpawner>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -28,16 +35,20 @@ public class PowerUp : MonoBehaviour
         stats.health *= multiplier;
         
         //Disable graphics for powerup (mesh renderer and collider)
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Collider>().enabled = false;
+        //GetComponent<MeshRenderer>().enabled = false;
+        //GetComponent<Collider>().enabled = false;
+        
+        // Remove the pickup from the spawner tracking list and destroy it instantly
+        if (randomSpawner != null)
+        {
+            randomSpawner.RemovePickup(transform.parent.gameObject);
+        }
+        Destroy(transform.parent.gameObject); // Destroy the pickup instantly
 
         //Wait x amount of seconds
         yield return new WaitForSeconds(duration);
 
         //Reverse the effect on player
         stats.health /= multiplier;
-
-        //Remove power up object
-        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject pickUpPrefab;
+    public int maxSpawnedPickups = 3; // Max spawned pickups at a time
+
+    private List<GameObject> spawnedPickups = new List<GameObject>();
+
+    void Start()
+    {
+        StartCoroutine(SpawnPickupRoutine());
+    }
+
+    IEnumerator SpawnPickupRoutine()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(2f, 4f);
+            yield return new WaitForSeconds(waitTime);
+
+            // Check if the number of spawned pickups is below the limit
+            if (spawnedPickups.Count < maxSpawnedPickups)
+            {
+                Vector3 randomSpawnPosition = new Vector3(Random.Range(-15, 21), -10, Random.Range(-19, 17));
+                GameObject newPickup = Instantiate(pickUpPrefab, randomSpawnPosition, Quaternion.identity);
+
+                // Add the new pickup to the list
+                spawnedPickups.Add(newPickup);
+            }
+        }
+    }
+
+    // Optional: Call this method when a pickup is collected/destroyed
+    public void RemovePickup(GameObject pickup)
+    {
+        spawnedPickups.Remove(pickup);
+        Destroy(pickup);
+    }
+}
